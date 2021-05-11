3d283a6a26c871c09212271d30812ae7d25c9283
Christopher Siem <65181238+christophersiem@users.noreply.github.com>
2021-05-11 13:59:48 +0200

    Image copy to clipboard (#6)
    * Add button to copy link to clipboard
    
    * Change State name

--- src/components/ShareableLinks.tsx
+++ src/components/ShareableLinks.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro'
+import { useState } from 'react'
 
 interface ShareableLinkProps {
   rooms: string[]
@@ -6,23 +7,43 @@ interface ShareableLinkProps {
 }
 
 export default function ShareableLinks({ rooms, groups }: ShareableLinkProps) {
+
+  const [showCopySuccessMessage, setShowCopySuccessMessage] = useState(false)
+
   const roomString = toUriEncodedJson(rooms)
   const groupsString = toUriEncodedJson(groups)
-
   const query = `?rooms=${roomString}&groups=${groupsString}`
-
   const appUrl = 'share' + query
-
   const imageUrl = 'api/image' + query
+  const imageUrlClipboard = window.location.host + '/' + imageUrl
+
+  const activateSuccessMessage = () => {
+    setShowCopySuccessMessage(true)
+    setTimeout(() => {
+      setShowCopySuccessMessage(false)
+    }, 2000)
+  }
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(imageUrlClipboard).then(() => activateSuccessMessage())
+  }
 
   return (
     <Wrapper>
-      <Link href={appUrl} target="_blank" rel="noreferrer noopener">
+      <Link href={appUrl} target='_blank' rel='noreferrer noopener'>
         Shareable link
       </Link>
-      <Link href={imageUrl} target="_blank" rel="noreferrer noopener">
-        Link to image
-      </Link>
+      <div>
+        <Link href={imageUrl} target='_blank' rel='noreferrer noopener'>
+          Link to image
+        </Link>
+        <Button
+          onClick={handleCopyClick}
+        >
+          🔗
+        </Button>
+        {showCopySuccessMessage && <SuccessMessage>✅ copied</SuccessMessage>}
+      </div>
     </Wrapper>
   )
 }
@@ -30,6 +51,18 @@ export default function ShareableLinks({ rooms, groups }: ShareableLinkProps) {
 function toUriEncodedJson(obj: any) {
   return encodeURI(JSON.stringify(obj))
 }
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+`
+
+const SuccessMessage = styled.p`
+  display: inline;
+  font-size: 0.75em;
+  padding-left: 5px;
+`
 
 const Wrapper = styled.section`
   display: flex;
